3a7b102c2798d1e658ce378fb403da008455d179
Charge987 <joaolucassilvatersariol@gmail.com>
2023-02-14 14:24:46 +0000

    Strogonoff

--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -2,7 +2,7 @@ public class EstruturaDeSelecao {
     public static void main(String[] args){
         int idade;
 
-        idade=17;
+        idade=21;
         if (idade >= 18)
         {
             System.out.print( "apto ao Serviço Militar Obrigatorio!");
