7a189e76302ef753debe43a6b7b6ace299f89058
Charge987 <joaolucassilvatersariol@gmail.com>
2023-02-28 13:59:00 +0000

    strogonoff

--- MediaDeNotas.java
+++ MediaDeNotas.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
 public class MediaDeNotas {
     
-
-public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    int n1, n2, media;
-    System.out.println("Informe o valor da sua primeira nota");
-    n1 = input.nextInt();
-   
-    System.out.println( "Informe o valor de sua segunda nota " );
-    n2 = input.nextInt();
-
-    media  = (n1 + n2) /2 ; 
-    System.out.printf(" Sua primeira nota foi =" + n1);
-        System.out.printf("\n Segunda nota foi  =" + n2);
-        System.out.printf("\n Sua média é =" + media  + "\n"); 
-        if (media >= 8 && media <= 10)
-        {
-            System.out.print( "Aluno está aprovado" \n);
-
-        } else if (media >= 0 && media <= 7){
-            System.out.print( "Aprovado, mas terá recuperação" \n);
-        }else if (media >= 0 && media <= 4){
-            System.out.print( "Aluno reprovado" \n);
-        }else{
-            System.out.print( "Erro na média" \n);
-
-        }
-}
-}
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n1, n2, media;
+    
+        System.out.println( "informe o valor da primeira nota:");
+        n1 = input.nextInt();
+    
+        System.out.println ("Informe o valor da segunda nota:");
+        n2 = input.nextInt();
+    
+        media = (n1 + n2)  /2;
+    
+        System.out.printf("Sua primeira nota foi =" + n1);
+        System.out.printf(" \n Sua segunda nota foi =" + n2);
+        System.out.printf(" \n Sua média é foi = " + media + "\n");
+    
+    
+    
+    
+    }
+    
+    
+    }
