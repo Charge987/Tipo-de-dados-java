8f587988694ab30909409943d9ea8eee404aa5a9
Charge987 <joaolucassilvatersariol@gmail.com>
2023-03-06 11:40:25 +0000

    strogonoff

---
+++ Tarefa2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Tarefa2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Double P,A, IMC;
+
+
+System.out.println("Informe sua Altura  :");
+nome = input.next();
+
+System.out.println("Informe seu Peso :");
+idade = input.nextDouble();
+
+IMC = P / ( A * A);
+
+ if( IMC <= 18.5 )
+    {
+    System.out.print( "Abaixo do Peso");
+    }
+    else if( IMC  ){
+
+    }
+    else if
+
+}
+}
