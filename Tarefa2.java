850ab210876f34e39a700fb40fadb8e3543cf8e2
Charge987 <joaolucassilvatersariol@gmail.com>
2023-03-07 14:20:57 +0000

    salvar

--- Tarefa2.java
+++ Tarefa2.java
@@ -5,22 +5,33 @@ public class Tarefa2 {
         Double P,A, IMC;
 
 
-System.out.println("Informe sua Altura  :");
-nome = input.next();
+    System.out.println("Informe sua Altura  :");
+    A = input.nextDouble();
 
-System.out.println("Informe seu Peso :");
-idade = input.nextDouble();
+    System.out.println("Informe seu Peso :");
+    P = input.nextDouble();
 
-IMC = P / ( A * A);
+    IMC = P / ( A * A);
 
- if( IMC <= 18.5 )
-    {
-    System.out.print( "Abaixo do Peso");
-    }
-    else if( IMC  ){
+    if( IMC <= 18.5 )
+        {
+        System.out.print( "Abaixo do Peso");
+        }
+        else if( IMC >=18.5  && IMC <=24.9){
+            System.out.print(  "Peso Normal");
+        }
+        else if( IMC >=25.0 && IMC <= 29.9){
+            System.out.print(  "Sobrepeso");
+        }
+        else if( IMC >=30.0 && IMC <= 34.9){
+            System.out.print(  "Obesidade grau 1");
+        }
+        else if( IMC >=35.0 && IMC <= 39.9){
+            System.out.print(  "Obesidade grau 2");
+        } else{
+        System.out.print(  "Obesidade grau 3");
+        }
 
-    }
-    else if
 
-}
+    }         
 }
