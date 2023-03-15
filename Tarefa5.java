97e83fab479d9bc289c3c37f938df66692db1fa9
Charge987 <joaolucassilvatersariol@gmail.com>
2023-03-15 17:02:29 +0000

    strogonoff

---
+++ Tarefa5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Tarefa5 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("Informe o numero  :");
+        n = input.nextInt();
+    
+    if (n == 1){
+        System.out.print( "Segunda-feira");
+    }else if (n == 2){
+        System.out.print( "Terça-feira");
+    }else if (n == 3){
+        System.out.print( "Quarta-feira");
+    }else if (n == 4){
+        System.out.print( "Quinta-feira");
+    }else if (n == 5){
+            System.out.print( "Sexta-feira");
+    }else if (n == 6){
+        System.out.print("Sabado");
+    }else if(n == 7){
+        System.out.print("Domingo");
+    }
+    
+    }    
+    
+
+
+
+    }    
